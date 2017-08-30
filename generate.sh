9f6501a5914638d4bdf2a7967efe73744200ccdf
Jonas Oberg <jonas@ffkp.se>
2017-08-30 14:29:07 +0200

    Specific path for output file

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 sed '/<!--START_HTML-->/,/<!--END_HTML-->/d' < /usr/share/blog/content/page.md \
  | sed -e 's/<\(a\|br\)\s[^>]*>//g' \
  | sed -e 's/<\/[^>]*>//g' \
- | sed '/+++/,/+++/d' > page-pdf.md
+ | sed '/+++/,/+++/d' > /page-pdf.md
