779da27c47d142322d243e631926ce2a8f880666
Jonas Oberg <jonas@ffkp.se>
2017-10-27 13:14:31 +0200

    Initial commit of new site, waiting for video.

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sed '/<!--START_HTML-->/,/<!--END_HTML-->/d' < /usr/share/blog/content/page.md \
+sed '/<!--START_HTML-->/,/<!--END_HTML-->/d' < /usr/share/blog/content/practices.md \
  | sed -e 's/<\(a\|br\)\s[^>]*>//g' \
  | sed -e 's/<\/[^>]*>//g' \
  | sed '/+++/,/+++/d' > /page-pdf.md
